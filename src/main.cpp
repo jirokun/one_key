f38a0e2de86d09f6358539aa099574d50142004a
Jiro Iwamoto <jirokun@gmail.com>
2021-11-26 20:35:21 +0900

    実用できる形になったもの

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,72 @@
-/**
- * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
+/*
+ * ATOM LiteをBLEキーボードにする。
+ *
+ * 送信するキーを変更するにはSEND_KEYの値を変更する。
  */
-#include <Arduino.h>
-#include <BleKeyboard.h>
 
-BleKeyboard bleKeyboard;
+#include <BleKeyboard.h> // https://github.com/T-vK/ESP32-BLE-Keyboard
+#include <M5Atom.h>
+
+const uint8_t SEND_KEY = 0x0a;
+
+const std::string DEVICE_NAME("ATOM Lite BLE Keyboard");
+const std::string DEVICE_MANUFACTURER("M5Stack");
+
+const CRGB CRGB_BLE_CONNECTED(0x00, 0x00, 0xf0);
+// 数値は緑だが Lite では赤く光る https://github.com/m5stack/M5Atom/issues/5
+const CRGB CRGB_BLE_DISCONNECTED(0x00, 0xf0, 0x00);
+
+BleKeyboard bleKeyboard(DEVICE_NAME, DEVICE_MANUFACTURER);
+bool isBleConnected = false;
+bool isKeyPressed = false;
+unsigned long pressBeginTime = 0;
+unsigned long pressEndTime = 0;
+
+CRGB dispColor(uint8_t g, uint8_t r, uint8_t b) {
+  return (CRGB)((g << 16) | (r << 8) | b);
+}
 
 void setup() {
-  //Serial.begin(115200);
-  Serial.println("Starting BLE work!");
+  M5.begin(true, false, true); // Serial: Enable, I2C: Disable, Display: Enable
   bleKeyboard.begin();
+  M5.dis.drawpix(0, 0xf00000);
+  Serial.println(DEVICE_NAME.c_str());
 }
 
 void loop() {
-  if(bleKeyboard.isConnected()) {
-    Serial.println("Sending 'Hello world'...");
-    bleKeyboard.print("Hello world");
-
-    delay(1000);
-
-    Serial.println("Sending Enter key...");
-    bleKeyboard.write(KEY_RETURN);
-
-    delay(1000);
-
-    Serial.println("Sending Play/Pause media key...");
-    bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
-
-    delay(1000);
-
-    Serial.println("Sending Ctrl+Alt+Delete...");
-    bleKeyboard.press(KEY_LEFT_CTRL);
-    bleKeyboard.press(KEY_LEFT_ALT);
-    bleKeyboard.press(KEY_DELETE);
-    delay(100);
-    bleKeyboard.releaseAll();
+  M5.update();
+  if (bleKeyboard.isConnected()) {
+    if (!isBleConnected) {
+      M5.dis.drawpix(0, 0xf00000);
+      isBleConnected = true;
+      Serial.println("Connected");
+    }
+    if (isKeyPressed) {
+      if (M5.Btn.isReleased()) {
+        isKeyPressed = false;
+        Serial.println("Released");
+        pressEndTime = millis();
+        unsigned long pressTime = pressEndTime - pressBeginTime;
+        if (pressTime >= 3000 && pressTime < 10000) {
+          modeChange();
+        }
+      }
+    } else {
+      if (M5.Btn.isPressed()) {
+        bleKeyboard.press(SEND_KEY);
+        bleKeyboard.release(SEND_KEY);
+        isKeyPressed = true;
+        Serial.println("Pressed");
+        pressBeginTime = millis();
+      }
+    }
+  } else {
+    if (isBleConnected) {
+      M5.dis.drawpix(0, dispColor(0, 1, 0));
+      isBleConnected = false;
+      isKeyPressed = false;
+      Serial.println("Disconnected");
+    }
   }
-
-  Serial.println("Waiting 5 seconds...");
-  delay(5000);
+  delay(100);
 }
